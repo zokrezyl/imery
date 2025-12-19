0764014c7676999b09eb641eca44995aeb992c1b
zokrezyl <zokrezyl@gmail.com>
2025-12-19 17:01:38 +0100

    fixind pyodide: imguizmo basics

--- docs/demo/js/pyodide_loader.js
+++ docs/demo/js/pyodide_loader.js
@@ -1,7 +1,7 @@
 // js/pyodide.js
 
 // Imery version - updated by build process
-const IMERY_VERSION = "0.0.52";
+const IMERY_VERSION = "0.0.53";
 
 async function load_pyodide_imgui_render() {
     console.log('Loading load_pyodide_imgui_render.py');
